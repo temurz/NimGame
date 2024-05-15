cd9e666219d371c7199287a50fadb74ed289830b
Temur <timurzaitov1998@gmail.com>
2024-05-15 16:13:05 +0200

    Change implementation to saving the mex values from 0 until the input number

--- NimGame/main.swift
+++ NimGame/main.swift
@@ -13,99 +13,50 @@ import Foundation
 print("Print some integer value!")
 let input = Int(readLine() ?? "")
 
-var mexValues = [Int: Int]()
-var mySet = Set<Int>()
-var globalSet = Set<Int>()
-
+var mexDictionary = [Int: Int]()
 
 if input == nil {
     print("Type Int value")
 }else {
-    _ = mainCalculation(num: [input!])
-    print(calculateMex(for: globalSet) == 0 ? 0 : calculateMex(for: globalSet) - 1)
+    calculateWithIteration(for: input!)
 }
 
-
-func mainCalculation(num: [Int]) -> Int {
-    if num.count == 1 {
-        let n = num[0]
-        switch n {
-        case 0:
-            mexValues[0] = 0
-            mySet.insert(0)
-            return 0
-        case 1:
-            mexValues[1] = 0
-            mySet.insert(0)
-            return 0
-        case 2:
-            mexValues[2] = 0
-            mySet.insert(0)
-            return 0
-        default:
-            break
+func calculateWithIteration(for num: Int) {
+    for i in 0 ... num {
+        var newSet = Set<Int>()
+        if i - 1 >= 2 {
+            for j in 0 ..< (i-1)/2 {
+                newSet.insert(
+                    (mexDictionary[j+1] ?? 0)^(mexDictionary[i-2-j] ?? 0)
+                )
+            }
         }
         
-        let first = n - 1
-        if first > 0 {
-            if let mex = mexValues[first] {
-                mySet.insert(mex)
-            }else {
-                for i in 0 ..< first / 2 {
-                    mySet.insert(mainCalculation(num: [i+1, first-(i+1)]))
+        if i - 2 > 0 {
+            newSet.insert(mexDictionary[i-2] ?? 0)
+            if i - 2 >= 2 {
+                for j in 0 ..< (i-2)/2 {
+                    newSet.insert(
+                        (mexDictionary[j+1] ?? 0)^(mexDictionary[i-3-j] ?? 0)
+                    )
                 }
             }
         }
         
-        
-        
-        let second = n - 2
-        if second > 0 {
-            if let mex = mexValues[second] {
-                mySet.insert(mex)
-            }else {
-                for i in 0 ..< second / 2 {
-                    mySet.insert(mainCalculation(num: [i+1, second-(i+1)]))
-                }
-                let res = mainCalculation(num: [second])
-                mySet.insert(res)
-                mexValues[second] = res
-            }
+        if i - 3 == 0 {
+            newSet.insert(0)
         }
         
-        
-        
-        let third = n - 3
-        if third >= 0 {
-            if let mex = mexValues[third] {
-                mySet.insert(mex)
-            }else {
-                for i in 0 ..< third / 2 {
-                    mySet.insert(mainCalculation(num: [i+1, third-(i+1)]))
-                }
+        if i - 3 >= 2 {
+            for j in 0 ..< (i-3)/2 {
+                newSet.insert(
+                    (mexDictionary[j+1] ?? 0)^(mexDictionary[i-4-j] ?? 0)
+                )
             }
-            
         }
-    }else {
-        let n1 = num[0]
-        let n2 = num[1]
-        
-        let res = xor(
-            first: mainCalculation(num: [n1]),
-            second: mainCalculation(num: [n2])
-        )
-        mySet = Set()
-        globalSet.insert(res)
-        return res
+        mexDictionary[i] = calculateMex(for: newSet)
+        print("\(i) mex is \(mexDictionary[i] ?? 0)")
     }
-    globalSet.insert(calculateMex(for: mySet))
-    return calculateMex(for: mySet)
-}
-
-
-
-func xor(first: Int, second: Int) -> Int {
-    return first == second ? 0 : 1
 }
 
 func calculateMex(for set: Set<Int>) -> Int {
